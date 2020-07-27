3e8e15e7c1d0eaa2d62333dd389f8ff24fe00347
ViridityMoon <viriditymoon@gmail.com>
2020-07-27 11:42:44 -0400

    Cleaning up actions and action creators for reusability

--- app/src/actions/index.js
+++ app/src/actions/index.js
@@ -1,39 +1,32 @@
 import { axiosWithAuth } from '../utils';
 
 export const FETCH_TODO = 'FETCH_TODO';
-export const FETCH_TODO_SUCCESS = 'FETCH_TODO_SUCCESS';
-export const FETCH_TODO_FAILURE = 'FETCH_TODO_FAILURE';
 export const DELETE_TODO = 'DELETE_TODO';
-export const DELETE_TODO_SUCCESS = 'DELETE_TODO_SUCCESS';
-export const DELETE_TODO_FAILURE = 'DELETE_TODO_FAILURE';
 export const ADD_TODO = 'ADD_TODO';
-export const ADD_TODO_SUCCESS = 'ADD_TODO_SUCCESS';
-export const ADD_TODO_FAILURE = 'ADD_TODO_FAILURE';
 export const EDIT_TODO = 'EDIT_TODO';
-export const EDIT_TODO_SUCCESS = 'EDIT_TODO_SUCCESS';
-export const EDIT_TODO_FAILURE = 'EDIT_TODO_FAILURE';
+export const ACTION_SUCCESS = 'ACTION_SUCCESS';
+export const ACTION_FAILURE = 'ACTION_FAILURE';
 
-
-export const TOGGLE_ACTIVE = 'TOGGLE_ACTIVE';
-
-
+export const TOGGLE_EDITING = 'TOGGLE_EDITING';
+export const TOGGLE_ADDING = 'TOGGLE_ADDING';
+export const TOGGLE_COMPLETED = 'TOGGLE_COMPLETED';
 
 
 export const fetchToDo = () => {
     // I'm going to show one of you how to do this, you'll need a GET request for MVP.
 };
 
-export const removeToDo = () => {
+export const deleteToDo = () => {
     return dispatch => {
       dispatch({ type: DELETE_TODO});
       axiosWithAuth()
         .delete('')
         .then(res => {
-          // dispatch({ type: DELETE_TODO_SUCCESS, payload: res.data});
+          // dispatch({ type: ACTION_SUCCESS, payload: res.data});
         })
         .catch(err => {
           console.log(err);
-          // dispatch({ type: DELETE_TODO_FAILURE, payload: err.data });
+          // dispatch({ type: ACTION_FAILURE, payload: err.data });
       });
   };
 };
@@ -45,11 +38,11 @@ export const addToDo = () => {
       .post('')
       .then(res => {
         console.log(res);
-        // dispatch({ type: ADD_TODO_SUCCESS, payload: res.data});
+        // dispatch({ type: ACTION_SUCCESS, payload: res.data});
       })
       .catch(err => {
         console.log(err);
-        // dispatch({ type: ADD_TODO_FAILURE, payload: err.data });
+        // dispatch({ type: ACTION_FAILURE, payload: err.data });
     });
   };
 };
@@ -61,15 +54,23 @@ export const editToDo = () => {
       .put('')
       .then(res => {
         console.log(res);
-        // dispatch({ type: EDIT_TODO_SUCCESS, payload: res.data});
+        // dispatch({ type: ACTION_SUCCESS, payload: res.data});
       })
       .catch(err => {
         console.log(err);
-        // dispatch({ type: EDIT_TODO_FAILURE, payload: err.data });
+        // dispatch({ type: ACTION_FAILURE, payload: err.data });
     });
   };
 };
 
-export const toggleActive = () => {
-  return { type: TOGGLE_ACTIVE }
+export const toggleAdding = () => {
+  return { type: TOGGLE_EDITING }
+};
+
+export const toggleEditing = () => {
+  return { type: TOGGLE_ADDING }
+};
+
+export const toggleCompleted = () => {
+  return { type: TOGGLE_COMPLETED }
 };
