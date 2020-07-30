1ec50bdd856fa1c1ae1098daf565bf8339203d32
Myeongwon André Jeon <andrejeon@Myeongwons-MBP.fios-router.home>
2020-07-30 09:20:41 -0400

    added map

--- app/src/components/ToDoList.js
+++ app/src/components/ToDoList.js
@@ -49,6 +49,15 @@ function ToDoList (props) {
   return (
       <div className="ToDoList">
         { isEditing ? <ListEditForm setIsEditing={setIsEditing} list={props.list.listname} id={props.list.id}/> : <h2 onClick={toggleEditing}>{props.list.listname}</h2>}
+        {
+          toDos.map( task => {
+            return (
+              // <div onClick={markCompleted}>
+                <ToDo key={task.id} task={task}/>
+              // </div>
+            )
+          })
+        }
         <button onClick={deleteList}>X</button>
         <button onClick={addNewToDo}>New Todo</button>
       </div>
